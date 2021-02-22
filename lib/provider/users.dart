74ba898c96ff05f4462093eaa10c170b147793c6
paulosergioduff <paulosergioduff@gmail.com>
2021-02-22 14:04:56 -0300

    Permissão de internet e pacotes necessarios

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -1,10 +1,14 @@
 import 'dart:math';
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/data/dummy_users.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:http/http.dart' as http;
 
 class Users with ChangeNotifier {
+  static const _baseUrl =
+      'https://fireflutter-d1493-default-rtdb.firebaseio.com/';
+
   final Map<String, User> _items = {...DUMMY_USERS};
 
   List<User> get all {
@@ -19,7 +23,9 @@ class Users with ChangeNotifier {
     return _items.values.elementAt(i);
   }
 
-  void put(User user) {
+// Alteração de dos dados
+
+  Future<void> put(User user) async {
     if (user == null) {
       return;
     }
@@ -37,7 +43,20 @@ class Users with ChangeNotifier {
         ),
       );
     } else {
-      final id = Random().nextDouble().toString();
+// Parte do código que insere dados
+
+      final response = await http.post(
+        "$_baseUrl/users.json",
+        body: json.encode({
+          'name': user.name,
+          'email': user.email,
+          'avatarUrl': user.avatarUrl,
+        }),
+      );
+
+      final id = json.decode(response.body)['name'];
+      print(json.decode(response.body));
+
       _items.putIfAbsent(
         id,
         () => User(
@@ -52,7 +71,7 @@ class Users with ChangeNotifier {
   }
 
   void remove(User user) {
-    if(user != null && user.id != null) {
+    if (user != null && user.id != null) {
       _items.remove(user.id);
       notifyListeners();
     }
